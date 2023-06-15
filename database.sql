474d2d8bd542a32c4908ecc45970d82a86ef586a
Touathi Vang <Touathi@outlook.com>
2023-06-15 14:25:14 -0500

    added dummy data

--- database.sql
+++ database.sql
@@ -19,3 +19,8 @@ CREATE TABLE "favorite" (
 	"giphy_name" VARCHAR(200),
 	"giphy_url" VARCHAR(200),
 	"category_id" integer REFERENCES "category");
+
+
+-- dummy test data
+INSERT INTO "favorite" ("giphy_name", "giphy_url")
+VALUES ('test_giphy', 'https://media1.giphy.com/media/6A4ZswXnm3FnH4JDUm/giphy.gif?cid=516fc6962poqmlzkxajwg5l3bdqo01e1que43wdg2e2guks7&ep=v1_gifs_trending&rid=giphy.gif&ct=g');
