4046d034a92695e066954cf20fa309fff45c1fac
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-06-15 10:54:22 -0500

    created the basic biolerplate for index.jsx

--- src/index.js
+++ src/index.js
@@ -1,10 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeLatest, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+const sagaMiddleware = createSagaMiddleware();
+
+function* rootSaga() {
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const store = createStore(
+    combineReducers({}),
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+      <Provider store={store}>
         <App />
+      </Provider>
     </React.StrictMode>
 );
