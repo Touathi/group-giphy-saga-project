3d826186c33bfc908bc8056ae61aab498d44f201
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-06-15 12:13:24 -0500

    created a new router for the search request git

---
+++ server/routes/search.routerr.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const pool = require('../modules/pool');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  // return giphy api request
+});
+
+module.exports = router;
+
+
+
+
+
